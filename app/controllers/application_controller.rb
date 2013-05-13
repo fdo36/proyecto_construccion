9d2eee0365be52e61862a729b7cbcc0268e6bdb3
Felipe Astroza <felipe@astroza.cl>
2013-05-13 02:01:39 -0400

    El acceso a /admin/users requiere q seas superadmin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
       "application"
     end
   end
+  
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => exception.message
+  end
 end
