2afd0d652f1c9ba13f4e1a2491134b46eae71b96
Felipe Astroza <felipe@astroza.cl>
2013-05-12 22:31:13 -0400

    El login ahora usa otro layout (distinto de la aplicacion general)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  protected
+  def layout_by_resource
+    if devise_controller?
+      "devise_layout"
+    else
+      "application"
+    end
+  end
 end
