233672eb45ecf5f5bfa595939ecaf8595d3e92ab
Daniel Mella <dmella04@gmail.com>
2013-05-15 02:47:14 -0400

    Mejora de la identificación de errores

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -61,7 +61,7 @@ class CompaniesController < ApplicationController
 
     respond_to do |format|
       if @company.update_attributes(params[:company])
-        format.html { redirect_to @company, notice: 'La compañia fue editada correctamente.' }
+        format.html { redirect_to @company, notice: 'La compania fue editada correctamente.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
