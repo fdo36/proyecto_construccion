28b841bd13416659512628ccd5770d87010aa413
Felipe Navarro <felipe.navarro22@gmail.com>
2013-05-14 22:32:26 -0400

    validadores y arreglos generales

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -45,7 +45,7 @@ class CompaniesController < ApplicationController
 
     respond_to do |format|
       if @company.save
-        format.html { redirect_to @company, notice: 'Company was successfully created.' }
+        format.html { redirect_to @company, notice: 'La compania fue editada exitosamente.' }
         format.json { render json: @company, status: :created, location: @company }
       else
         format.html { render action: "new" }
