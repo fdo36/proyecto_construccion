48123bc1f92b968afb069f78b78d8677ff979859
Felipe Navarro <felipe.navarro22@gmail.com>
2013-05-21 22:51:32 -0400

    arreglo en validaciones company

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,6 +3,8 @@ class Company < ActiveRecord::Base
   validates :name, :presence => true
   validates :rut, :presence => true
   validates :email, :presence => true
+  validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i,
+    :message => "debe seguir el formato ejemplo@midominio.com" }
   attr_accessible :address, :city, :email, :line_of_business, :name, :phone, :rut , :active
   
   has_many :users
