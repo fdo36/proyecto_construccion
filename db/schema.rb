ccd2accb29ae46ce188b7d409cb9e39498014a33
Felipe Astroza <felipe@astroza.cl>
2013-05-29 04:21:50 -0400

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130528203655) do
+ActiveRecord::Schema.define(:version => 20130529073823) do
 
   create_table "access_rights", :force => true do |t|
     t.string   "model_name"
@@ -66,9 +66,20 @@ ActiveRecord::Schema.define(:version => 20130528203655) do
     t.string   "phone"
     t.text     "contact"
     t.boolean  "active"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.boolean  "is_deleted"
+  end
+
+  create_table "dispatch_containers", :force => true do |t|
+    t.integer  "dispatch_id"
+    t.integer  "container_id"
+    t.integer  "quality_id"
+    t.integer  "variety_id"
+    t.integer  "quantity"
+    t.float    "gross_weight"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
-    t.boolean  "is_deleted"
   end
 
   create_table "dispatches", :force => true do |t|
@@ -76,8 +87,6 @@ ActiveRecord::Schema.define(:version => 20130528203655) do
     t.integer  "destination_id"
     t.integer  "kind_id"
     t.datetime "dispatch_datetime"
-    t.integer  "user_id"
-    t.integer  "company_id"
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
   end
@@ -167,7 +176,6 @@ ActiveRecord::Schema.define(:version => 20130528203655) do
     t.datetime "updated_at",     :null => false
   end
 
->>>>>>> c6bddc725d7eaa7799a5a23bd9f054eb3bbab944
   create_table "producers", :force => true do |t|
     t.string   "rut"
     t.string   "name"
@@ -182,12 +190,35 @@ ActiveRecord::Schema.define(:version => 20130528203655) do
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
     t.boolean  "is_deleted"
+    t.integer  "locality_id"
+    t.integer  "code"
   end
 
   create_table "qualities", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "receipt_containers", :force => true do |t|
+    t.integer  "receipt_id"
+    t.integer  "container_id"
+    t.integer  "quality_id"
+    t.integer  "variety_id"
+    t.float    "price_kg"
+    t.integer  "quantity"
+    t.float    "gross_weight"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
+
+  create_table "receipts", :force => true do |t|
+    t.string   "code"
+    t.integer  "producer_id"
+    t.integer  "kind_id"
+    t.datetime "receipt_datetime"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
   end
 
   create_table "regions", :force => true do |t|
@@ -252,6 +283,9 @@ ActiveRecord::Schema.define(:version => 20130528203655) do
     t.boolean  "super_admin"
     t.integer  "company_id"
     t.string   "gender"
+    t.string   "address"
+    t.integer  "region_id"
+    t.integer  "commune_id"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
@@ -294,6 +328,14 @@ ActiveRecord::Schema.define(:version => 20130528203655) do
   add_foreign_key "pallets", "varieties", :name => "pallets_variety_id_fk"
 
   add_foreign_key "producers", "communes", :name => "producers_commune_id_fk"
+
+  add_foreign_key "receipt_containers", "containers", :name => "receipt_containers_container_id_fk"
+  add_foreign_key "receipt_containers", "qualities", :name => "receipt_containers_quality_id_fk"
+  add_foreign_key "receipt_containers", "receipts", :name => "receipt_containers_receipt_id_fk"
+  add_foreign_key "receipt_containers", "varieties", :name => "receipt_containers_variety_id_fk"
+
+  add_foreign_key "receipts", "kinds", :name => "receipts_kind_id_fk"
+  add_foreign_key "receipts", "producers", :name => "receipts_producer_id_fk"
 
   add_foreign_key "roles_users", "roles", :name => "roles_users_role_id_fk"
   add_foreign_key "roles_users", "users", :name => "roles_users_user_id_fk"
