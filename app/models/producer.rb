bc204497ee8c762cf00a4063884ce15d4e0c8b8b
Roxana Tapia <rotapia09@alumnos.utalca.cl>
2013-06-01 23:04:45 -0400

    ahora si

--- app/models/producer.rb
+++ app/models/producer.rb
@@ -12,7 +12,7 @@ class Producer < ActiveRecord::Base
   validates :email, :format => { :with => /\A(([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,}))?\z/i,
     :message => "debe seguir el formato ejemplo@midominio.com" }
   
-  validates :rut, :format => { :with => /^(\d{2}.\d{3}.\d{3}-)([a-zA-Z]{1}$|\d{1}$)/,
+  validates :rut, :format => { :with => /^(\d{1,3}.\d{3}.\d{3}-)([a-zA-Z]{1}$|\d{1}$)/,
     :message => "debe ingresar el formato válido. Ejemplo: 11.111.111-1" }
 
   validates :phone, :code, :format => { :with => /^-?((?:\d+|\d*)$)/,
