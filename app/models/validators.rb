579b307afef7034c0d58bbee5e7f9e714fd894ff
Fernando Torres <fernando.sk8r.7@gmail.com>
2013-06-12 17:28:44 -0400

    rut bug fixes

--- app/models/validators.rb
+++ app/models/validators.rb
@@ -1,37 +1,41 @@
 #encoding: utf-8
 class RutValidator < ActiveModel::Validator
   def validate(record)
-  	invalid = true
-  	t = (record.rut[0, record.rut.length - 2].delete(".")).to_i
-  	d = record.rut[record.rut.length - 1, record.rut.length]
-    v=1
-	s=0
-    for i in (2..9)
-	  if i == 8
-	    v=2
-	  else 
-	  	v+=1
-	  end
-      s+=v*(t%10)
-	  t/=10
-	end
-	s = 11 - s%11
-	if s == 11
-	  if d.to_i == 0
-	  	invalid = false
-	  end
-	elsif s == 10
-	  if d.casecmp("K") == 0
-	  	invalid = false
-	  end
-	else
-	  if d.to_i == s
-	  	invalid = false
-	  end	
-	end
+    if record.rut.empty?
+      record.errors[:base] << "debe ingresar un RUT"
+    else
+    	invalid = true
+    	t = (record.rut[0, record.rut.length - 2].delete(".")).to_i
+    	d = record.rut[record.rut.length - 1, record.rut.length]
+      v=1
+  	  s=0
+      for i in (2..9)
+    	  if i == 8
+    	    v=2
+    	  else 
+    	  	v+=1
+    	  end
+        s+=v*(t%10)
+    	  t/=10
+  	  end
+  	  s = 11 - s%11
+  	  if s == 11
+  	    if d.to_i == 0
+  	  	  invalid = false
+  	    end
+  	  elsif s == 10
+  	    if d.casecmp("K") == 0
+  	  	  invalid = false
+  	    end
+  	  else
+  	    if d.to_i == s
+  	  	  invalid = false
+  	    end	
+  	  end
 
-    if invalid == true
-      record.errors[:base] << "el RUT es inválido"
+      if invalid == true
+        record.errors[:base] << "el RUT es inválido"
+      end
     end
   end
 end
