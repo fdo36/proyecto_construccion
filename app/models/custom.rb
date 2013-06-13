d6254236918db05a4f552489d55b2ed797dd85a2
Fernando Torres <fernando.sk8r.7@gmail.com>
2013-06-13 10:40:01 -0400

    minor bug fixes

--- app/models/custom.rb
+++ app/models/custom.rb
@@ -14,7 +14,7 @@ class Custom < ActiveRecord::Base
   validates :rut, :format => { :with => /^(\d{1,3}.\d{3}.\d{3}-)([a-zA-Z]{1}$|\d{1}$)/,
     :message => "debe ingresar el formato válido. Ejemplo: 11.111.111-1" }
 
-  validates :phone, :code, :format => { :with => /^-?((?:\d+|\d*)$)/,
+  validates :phone, :format => { :with => /^-?((?:\d+|\d*)$)/,
     :message => "debe ingresar un número válido" }
 
 
