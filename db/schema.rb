965865fd14f0648c7d97f820e55267d5903fce70
Felipe Navarro <felipe.navarro22@gmail.com>
2013-07-01 01:29:48 -0400

    Mas actualizaciones inventario

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130630234831) do
+ActiveRecord::Schema.define(:version => 20130701044107) do
 
   create_table "access_rights", :force => true do |t|
     t.string   "model_name"
@@ -315,11 +315,11 @@ ActiveRecord::Schema.define(:version => 20130630234831) do
     t.string   "phone"
     t.string   "email"
     t.string   "address"
-    t.string   "commune"
-    t.string   "contacto"
     t.boolean  "active"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "commune_id"
+    t.string   "contact"
   end
 
   create_table "qualities", :force => true do |t|
@@ -454,8 +454,9 @@ ActiveRecord::Schema.define(:version => 20130630234831) do
     t.integer  "supply_id"
     t.integer  "quantity"
     t.integer  "company_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",            :null => false
+    t.datetime "updated_at",            :null => false
+    t.integer  "dispatch_guide_number"
   end
 
   create_table "supplies_returns", :force => true do |t|
