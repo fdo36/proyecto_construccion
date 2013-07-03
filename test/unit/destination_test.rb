2214940edf425d3556e8eecdda908b585738077a
Cristobal Rojas <cristorb@gmail.com>
2013-07-02 22:20:07 -0400

    Test Unitarios terminados de Varieties y test unitarios de destination sin completar

--- test/unit/destination_test.rb
+++ test/unit/destination_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class DestinationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	#test "the truth" do
+    #	assert true
+   	#end
+	
+	test "Destino sin nombre con rut" do
+  		destination = Destination.new
+  		destination.rut = '16.726.966-4'
+  		refute destination.save
+	end
+
+	test "Destino con nombre con rut" do
+  		destination = Destination.new
+  		destination.rut = '16.726.966-4'
+  		destination.name = 'Nombre'
+  		refute destination.save
+	end
 end
