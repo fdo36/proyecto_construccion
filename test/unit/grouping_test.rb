7fccbdf510561f3411f652f940d8982f5f7b7b98
Javier Muñoz <jamunoz@alumnos.utalca.cl>
2013-07-03 02:11:38 -0400

    einls

--- test/unit/grouping_test.rb
+++ test/unit/grouping_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class GroupingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	# test "the truth" do
+	#   assert true
+	# end
+
+	#refute = Ensures that test is false.
+	#assert = Ensures that test is true.
+
+	test "Grouping sin nombre y sin descripcion" do
+		grouping = Grouping.new
+		refute grouping.save
+	end
+
+	test "Grouping sin nombre y con descripcion" do
+		grouping = Grouping.new
+		grouping.description = 'Descripcion 1'
+		refute grouping.save
+	end
+
+	test "Grouping con nombre y sin descripcion" do
+		grouping = Grouping.new
+		grouping.name = 'Nombre 1'
+		refute grouping.save
+	end
+
+	test "Grouping con nombre y con descripcion" do
+		grouping = Grouping.new
+		grouping.name = 'Nombre 1'
+		grouping.description = 'Descripcion 1'
+		assert grouping.save
+	end
 end
--- test/unit/producer_test.rb
+++ test/unit/producer_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class ProducerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	# test "the truth" do
+	#   assert true
+	# end
+
+  	#refute = Ensures that test is false.
+	#assert = Ensures that test is true.
+
+	test "Rut no vacio" do
+		producer = Producer.new
+		producer.rut = '17.501.528-0'
+		producer.save
+		assert producer.errors.size == 8
+	end
+
 end
