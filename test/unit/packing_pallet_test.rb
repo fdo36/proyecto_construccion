4b74b6d559e347e676f6e1ec0df695478d99ffc8
Cristobal Rojas <cristorb@gmail.com>
2013-07-03 00:16:46 -0400

    test de packing pallets

--- test/unit/packing_pallet_test.rb
+++ test/unit/packing_pallet_test.rb
@@ -4,4 +4,10 @@ class PackingPalletTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+	test "" do
+  		packingPallet = Packing_Pallet.new
+  		packingPallet.save
+  		assert packingPallet.errors.size == 5
+  	end
 end
