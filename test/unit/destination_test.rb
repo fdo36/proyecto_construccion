e9fa465a3854b8b69863659d6bdbb2ceba438d1d
Cristobal Rojas <cristorb@gmail.com>
2013-07-02 23:48:38 -0400

    Test de Calidades y Destinos

--- test/unit/destination_test.rb
+++ test/unit/destination_test.rb
@@ -4,17 +4,11 @@ class DestinationTest < ActiveSupport::TestCase
 	#test "the truth" do
     #	assert true
    	#end
-	
-	test "Destino sin nombre con rut" do
-  		destination = Destination.new
-  		destination.rut = '16.726.966-4'
-  		refute destination.save
-	end
 
-	test "Destino con nombre con rut" do
+	test "Destino verificacion de todos sus campos" do
   		destination = Destination.new
   		destination.rut = '16.726.966-4'
-  		destination.name = 'Nombre'
-  		refute destination.save
+  		destination.save
+  		assert destination.errors.size == 6
 	end
 end
