e5e7598741d29b793aff222227507bc87a9eeea9
Roxana Tapia <rotapia09@alumnos.utalca.cl>
2013-07-09 18:02:19 -0400

    shows de mantenedores relacionados a inventarios, agregados al menu lateral en application (revisar)

--- app/models/supplies_loan.rb
+++ app/models/supplies_loan.rb
@@ -1,4 +1,11 @@
 class SuppliesLoan < ActiveRecord::Base
   attr_accessible :company_id, :quantity, :supply_id, :worker_id
 
+  #s = Supply.find(:supply_id)
+
+  #min = (Supply.all(:select => "minimum_stock", :conditions => ["id = ?", s.id]))[0].minimum_stock
+  #stock = (Supply.all(:select => "stock_ini", :conditions => ["id = ?", s.id]))[0].stock_ini
+
+  #validates :quantity, :numericality => { :greater_than_or_equal_to => min, :less_than_or_equal_to => stock }
+  
 end
