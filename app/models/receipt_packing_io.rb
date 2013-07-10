61d2fa127317e2cf50b75aab21a43f1aaa9f2885
Felipe Astroza <felipe@astroza.cl>
2013-07-10 18:03:28 -0400

    Agregue kind_id en receipt_packing_io

--- app/models/receipt_packing_io.rb
+++ app/models/receipt_packing_io.rb
@@ -1,5 +1,5 @@
 class ReceiptPackingIo < ActiveRecord::Base
   acts_as_heir_of :subprocess_io
-  attr_accessible :code, :comments, :dispatch_guide_number, :pack_type_id, :producer_id, :trazability_code, :packing_pallet_id, :worker_id, :direction
+  attr_accessible :code, :comments, :dispatch_guide_number, :pack_type_id, :producer_id, :trazability_code, :packing_pallet_id, :worker_id, :direction, :kind_id
   belongs_to :producer
 end
