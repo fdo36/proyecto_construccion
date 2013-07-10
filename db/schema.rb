30012a7c22d99c0603350ed014d5b5a7a46ae6fb
Sergio <ssilvar@alumnos.utalca.cl>
2013-07-10 13:28:29 -0400

    avances en despacho

--- db/schema.rb
+++ db/schema.rb
@@ -129,6 +129,12 @@ ActiveRecord::Schema.define(:version => 20130709194017) do
     t.string   "name_driver"
     t.string   "rut_driver"
     t.string   "patent"
+    t.string   "nave"
+    t.string   "reservation"
+    t.string   "stamp_number"
+    t.string   "thermograph"
+    t.string   "dispatch_guide"
+    t.string   "po_number"
     t.datetime "created_at",           :null => false
     t.datetime "updated_at",           :null => false
   end
