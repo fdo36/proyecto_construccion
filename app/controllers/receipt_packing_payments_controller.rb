87f7f7c099d6fb6c9a1c407d40cdd3d92276da04
Mario Sanhueza <msanhuezal@alumnos.utalca.cl>
2013-07-12 03:47:46 -0400

    agregue receipt packing payments para controlar los pagos de ingresos en packing

---
+++ app/controllers/receipt_packing_payments_controller.rb
@@ -0,0 +1,24 @@
+class ReceiptPackingPaymentsController < ApplicationController
+  # GET /receipt_packing_ios
+  # GET /receipt_packing_ios.json
+  def index
+    @receipt_packing_ios = ReceiptPackingIo.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @receipt_packing_ios }
+    end
+  end
+
+  def pay
+    @receipt_packing_io = ReceiptPackingIo.find(params[:id])
+    @receipt_packing_io.update_attribute(:paid, "true")
+    time2 = DateTime.current()
+    @receipt_packing_io.update_attribute(:payment_date, time2)
+    @receipt_packing_io.update_attribute(:editable, "false")
+    respond_to do |format|
+      format.html { redirect_to receipt_packing_payments_url }
+      format.json { head :no_content }
+    end
+  end  
+end
