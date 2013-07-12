5f427a67534c46a0d86da4290235cb727413c93f
Daniel Mella <dmella04@gmail.com>
2013-07-12 02:55:44 -0400

    Code para empty_packs

--- app/models/empty_packs_destination_move.rb
+++ app/models/empty_packs_destination_move.rb
@@ -1,5 +1,5 @@
 class EmptyPacksDestinationMove < ActiveRecord::Base
-  attr_accessible :company_id, :destination_id, :pack_option, :pack_type_id, :quantity
+  attr_accessible :company_id, :destination_id, :pack_option, :pack_type_id, :quantity, :code
   
   belongs_to :destination ,:dependent => :destroy
   has_one :pack_types
